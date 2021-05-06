df773b8d7435160a4069afad34866d97a5d16c47
adarshsingh2k <adarshsingh2k@gmail.com>
2021-05-06 20:26:04 +0530

    init

---
+++ src/components/Tasks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Task from './Task'
+
+
+
+
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+    return (
+      <>
+        {tasks.map((task, index) => (
+          <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+        ))}
+      </>
+    )
+  }
+Tasks.propTypes = {
+
+}
+
+export default Tasks
