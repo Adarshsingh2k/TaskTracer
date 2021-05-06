6b77351460e3c572c4345e62a6ae9ff2471e29ea
adarshsingh2k <adarshsingh2k@gmail.com>
2021-05-06 20:43:18 +0530

    init

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,7 @@ const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
-      <h5 style={{fontStyle:'italic',
-        fontSize='10px'
-        }}>Created with ❤️ by Adarsh Singh</h5>
+      <h5 >Created with ❤️ by Adarsh Singh</h5>
       <Link to='/'>Go Back</Link>
     </div>
   )
